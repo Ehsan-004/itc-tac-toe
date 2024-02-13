9238f91a36048bc122060fc9e07a79921fba1a55
Ehsan <ehsankholusi@gmail.com>
2024-02-13 04:09:18 +0330

    change how hole code works
    I changed how the main game process is
    it works better now

--- tic tac toe.py
+++ tic tac toe.py
@@ -1,146 +1,81 @@
 from random import randint, choice
 from time import sleep
 
+# -------------------------------------------------------------------
+
 game_page = [
-    # 1    2    3
-    ['-', '-', '-'], # a
-    ['-', '-', '-'], # b
-    ['-', '-', '-']  # c
+#    1    2    3
+    '_', '_', '_', # a
+    '_', '_', '_', # b
+    '_', '_', '_' # c
 ]
 
-# --------------------------------------------------------------------------------------------------------
+# -------------------------------------------------------------------
 
-def printer(game_p):
-    print("      1        2        3")
-    print("a","[",game_p[0][0] ,"]","[", game_p[0][1],"]","[",game_p[0][2],"]", sep="  ", end="\n\n")
-    print("b","[",game_p[1][0] ,"]","[", game_p[1][1],"]","[",game_p[1][2],"]", sep="  ", end="\n\n")
-    print("c","[",game_p[2][0] ,"]","[", game_p[2][1],"]","[",game_p[2][2],"]", sep="  ", end="\n\n")
-    print("-" * 40)
+def printer():
+    print(game_page[0], game_page[1], game_page[2])
+    print(game_page[3], game_page[4], game_page[5])
+    print(game_page[6], game_page[7], game_page[8])
 
-# --------------------------------------------------------------------------------------------------------
-    
+# -------------------------------------------------------------------
+
 def game():
-    printer(game_page)
-    print("|your turn|")
-    user_input = input("enter the position: ")
-    if user_input[0] == "a":
-        if user_input[1] == '1':
-            if game_page[0][0] == '-':
-                game_page[0][0] = 'O'
-            else:
-                print("wrong move!!!")
-                game()
-        if user_input[1] == '2':
-            if game_page[0][1]:
-                game_page[0][1] = 'O'
-            else:
-                print("wrong move!!!")
-                game()
-        if user_input[1] == '3':
-            if game_page[0][2]:
-                game_page[0][2] = 'O'
-            else:
-                print("wrong move!!!")
-                game()
+    printer()
+    while True:
+        user_move = input("enter number: ")
+        if game_page[int(user_move)-1] == "_":
+            game_page[int(user_move)-1] = "O"
+            break
+    result , winner = checker()
+    if result:
+        return winner
 
-    elif user_input[0] == "b":
-        if user_input[1] == '1':
-            if game_page[0][0] == '-':
-                game_page[0][0] = 'O'
-            else:
-                print("wrong move!!!")
-                game()
-        if user_input[1] == '2':
-            if game_page[0][1]:
-                game_page[0][1] = 'O'
-            else:
-                print("wrong move!!!")
-                game()
-        if user_input[1] == '3':
-            if game_page[0][2]:
-                game_page[0][2] = 'O'
-            else:
-                print("wrong move!!!")
-                game()
-
-    elif user_input[0] == "c":
-        if user_input[1] == '1':
-            if game_page[0][0] == '-':
-                game_page[0][0] = 'O'
-            else:
-                print("wrong move!!!")
-                game()
-        if user_input[1] == '2':
-            if game_page[0][1]:
-                game_page[0][1] = 'O'
-            else:
-                print("wrong move!!!")
-                game()
-        if user_input[1] == '3':
-            if game_page[0][2]:
-                game_page[0][2] = 'O'
-            else:
-                print("wrong move!!!")
-                game()
-
-    printer(game_page)
-
-    print("|computer turn|")
-
-    sleep(1.5)
-
-    comp_choose = False
-    while not comp_choose :
-        ind_khat_c = randint(0, 2) # index of the list in gamepage
-        ind_khane_c = randint(0, 2) 
-        khane_c = game_page[ind_khat_c][ind_khane_c]
-        if khane_c == '-':
-            game_page[ind_khat_c][ind_khane_c] = 'X'
-            comp_choose = True
-
-# --------------------------------------------------------------------------------------------------------
-
-winner = None
-
-while True:
-    game()
-
-    if game_page[0][0] == game_page[1][1] == game_page[2][2] == 'X':
-        winner = "computer"
-        break
-
-    elif game_page[0][0] == game_page[1][1] == game_page[2][2] == 'O':
-        winner = "user"
-        break
-
-    elif game_page[0][2] == game_page[1][1] == game_page[2][0] == 'X':
-        winner = "computer"
-        break
-
-    elif  game_page[0][2] == game_page[1][1] == game_page[2][0] == 'O':
-        winner = "user"
-
-    for i in range (3):
-        if (game_page[i][0] == "X" and game_page[i][1] == "X" and game_page[i][2] == "X"):
-            winner = "computer"
+    while True:
+        computer_choice = choice(game_page)
+        if computer_choice == "_":
+            game_page[game_page.index(computer_choice)] = "X"
             break
 
-        elif  (game_page[i][0] == "O" and game_page[i][1] == "O" and game_page[i][2] == "O"):
-            winner = "user"
-            break
+    result , winner = checker()
+    if result:
+        return winner
+    
+    return None
 
-    for i in range (3):
-        if (game_page[0][i] == "X" and game_page[1][i] == "X" and game_page[2][i] == "O"):
-            winner = "computer"
-            break
-        elif (game_page[0][i] == "O" and game_page[1][i] == "O" and game_page[2][i] == "X"):
-            winner = "user"
-            break
 
-# --------------------------------------------------------------------------------------------------------
+# -------------------------------------------------------------------
+
+def checker():
+    for i in range(0, 9, 3):
+        if game_page[i] != "_":
+            if game_page[i] == game_page[i+1] == game_page[i+2]:
+                return True , game_page[i]
+            
+    for i in range(3):
+        if game_page[i] != "_":
+            if game_page[i] == game_page[i+3] == game_page[i+6]:
+                return True , game_page[i]
+            
+    if game_page[0] != "_":
+        if game_page[0] == game_page[4] == game_page[8]:
+            return True , game_page[0]
         
-print("-" * 40)
+    if game_page[2] != "_":
+        if game_page[2] == game_page[4] == game_page[6]:
+            return True , game_page[0]
+        
+    return None , "_"
+    
+# -------------------------------------------------------------------
 
-print("end of game!!!")
+res = "no one"
+while True:
+    res = game()
+    if res != None:
+        break
 
-print(winner , "won the game!!!")
+printer()
+print(res, "is the winner!!!!")
+
+
+
